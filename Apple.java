0c05519765371682e6aa944f640137bd705f5c75
Paul Epps <paul@eppsnet.com>
2026-04-19 11:29:27 -0700

    Create Apple class

---
+++ Apple.java
@@ -0,0 +1,17 @@
+public class Apple {
+    private double weight;
+    private boolean isRotten;
+
+    public Apple(double weight, boolean isRotten) {
+        this.weight = weight;
+        this.isRotten = isRotten;
+    }
+    
+    public double getWeight() {
+        return weight;
+    }
+
+    public boolean isRotten() {
+        return isRotten;
+    }
+}
